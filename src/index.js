2be52d6eb7cc889fd4cf903116951ef14e5c9c92
istrelkina <istrelkina@gmail.com>
2022-05-08 20:24:22 +1000

    feat: add changing language

--- src/index.js
+++ src/index.js
@@ -1,4 +1,87 @@
 const { body } = document;
+const script = document.querySelector('script');
+let keys;
+let textarea;
+let arrLetters;
+let isCapsLock = false;
+let isLangRu = false;
+const arrLettersEn = Array(26).fill().map((v, i) => String.fromCharCode(i + 65));
+arrLettersEn.push('`');
+const arrLettersRu = Array(32).fill().map((v, i) => String.fromCharCode(i + 192));
+arrLettersRu.push('Ё');
+const arrLang = ['ShiftLeft', 'AltLeft'];
+const setLang = new Set(arrLang);
+let pressed = new Set();
+const objLangRu = {
+  Backquote: 'Ё',
+  KeyQ: 'Й',
+  KeyW: 'Ц',
+  KeyE: 'У',
+  KeyR: 'К',
+  KeyT: 'Е',
+  KeyY: 'Н',
+  KeyU: 'Г',
+  KeyI: 'Ш',
+  KeyO: 'Щ',
+  KeyP: 'З',
+  BracketLeft: 'Х',
+  BracketRight: 'Ъ',
+  KeyA: 'Ф',
+  KeyS: 'Ы',
+  KeyD: 'В',
+  KeyF: 'А',
+  KeyG: 'П',
+  KeyH: 'Р',
+  KeyJ: 'О',
+  KeyK: 'Л',
+  KeyL: 'Д',
+  Semicolon: 'Ж',
+  Quote: 'Э',
+  KeyZ: 'Я',
+  KeyX: 'Ч',
+  KeyC: 'С',
+  KeyV: 'М',
+  KeyB: 'И',
+  KeyN: 'Т',
+  KeyM: 'Ь',
+  Comma: 'Б',
+  Period: 'Ю',
+};
+const objLangEn = {
+  Backquote: '`',
+  KeyQ: 'Q',
+  KeyW: 'W',
+  KeyE: 'E',
+  KeyR: 'R',
+  KeyT: 'T',
+  KeyY: 'Y',
+  KeyU: 'U',
+  KeyI: 'I',
+  KeyO: 'O',
+  KeyP: 'P',
+  BracketLeft: '[',
+  BracketRight: ']',
+  KeyA: 'A',
+  KeyS: 'S',
+  KeyD: 'D',
+  KeyF: 'F',
+  KeyG: 'G',
+  KeyH: 'H',
+  KeyJ: 'J',
+  KeyK: 'K',
+  KeyL: 'L',
+  Semicolon: ';',
+  Quote: "'",
+  KeyZ: 'Z',
+  KeyX: 'X',
+  KeyC: 'C',
+  KeyV: 'V',
+  KeyB: 'B',
+  KeyN: 'N',
+  KeyM: 'M',
+  Comma: ',',
+  Period: '.'
+};
 
 function showKeyboard(data) {
   const dataArray = [];
@@ -6,8 +89,8 @@ function showKeyboard(data) {
   keyboardWrapper.classList.add('keyboard-wrapper');
   const keyboardText = document.createElement('div');
   keyboardText.classList.add('keyboard-text');
-  const input = document.createElement('textarea');
-  keyboardText.append(input);
+  textarea = document.createElement('textarea');
+  keyboardText.append(textarea);
   keyboardWrapper.append(keyboardText);
 
   const keyboard = document.createElement('div');
@@ -28,21 +111,182 @@ function showKeyboard(data) {
         const keyDiv = document.createElement('div');
         keyDiv.classList.add('keys');
         if (key.class) keyDiv.classList.add(key.class);
-        keyDiv.textContent = key.value;
+        if (arrLettersEn.includes(key.value)) {
+          keyDiv.textContent = key.value.toLowerCase();
+        } else {
+          keyDiv.textContent = key.value;
+        }
         keyDiv.dataset.key = key.datasetVal;
         keyboardRow.append(keyDiv);
       });
       keyboard.append(keyboardRow);
     });
+    const aboutOS = document.createElement('p');
+    aboutOS.textContent = 'The keyboard was created in Windows';
+    aboutOS.classList.add('lang');
+    keyboard.append(aboutOS);
+    const aboutLang = document.createElement('p');
+    aboutLang.textContent = 'Press left Shift + Alt to change language';
+    aboutLang.classList.add('lang');
+    keyboard.append(aboutLang);
+
     keyboardWrapper.append(keyboard);
-    body.append(keyboardWrapper);
+    script.before(keyboardWrapper);
+    keys = document.querySelectorAll('.keys');
   }
 }
 
 async function getData() {
-  const res = await fetch('data.json');
-  const data = await res.json();
-  showKeyboard(data);
+  fetch('data.json')
+    .then((response) => response.json())
+    .then((result) => {
+      showKeyboard(result);
+      textarea = document.querySelector('textarea');
+      textarea.focus();
+    });
 }
 
-window.onload = getData;
+(getData());
+
+// event keyboard
+
+function getKeyActive(event) {
+
+  if (!textarea.hasFocus) textarea.focus();
+  keys.forEach((element) => {
+    if (element.dataset.key === event.code) {
+      element.classList.add('active');
+      if (event.code === 'MetaLeft' || event.code === 'Tab') event.preventDefault();
+    }
+  });
+  pressed.add(event.code);
+  for (let code of setLang) {
+    if (!pressed.has(code)) {
+      return;
+    }
+  }
+  pressed.clear();
+  isLangRu ? isLangRu = false : isLangRu = true;
+
+  if (isLangRu) {
+    keys.forEach((element) => {
+      if (objLangEn[element.dataset.key])
+        if (isCapsLock) {
+          element.textContent = objLangRu[element.dataset.key];
+        } else {
+          element.textContent = objLangRu[element.dataset.key].toLowerCase();
+        }
+    })
+  } else {
+    keys.forEach((element) => {
+      if (objLangRu[element.dataset.key])
+        if (isCapsLock) {
+          element.textContent = objLangEn[element.dataset.key];
+        } else {
+          element.textContent = objLangEn[element.dataset.key].toLowerCase();
+        }
+    });
+  }
+}
+
+function getKeyRemove(event) {
+  keys.forEach((element) => {
+    if (element.dataset.key === event.code) {
+      element.classList.remove('active');
+      element.classList.add('remove');
+    }
+    setTimeout(() => {
+      element.classList.remove('remove');
+    });
+  });
+  pressed.delete(event.code);
+}
+document.addEventListener('keydown', getKeyActive);
+document.addEventListener('keyup', getKeyRemove);
+
+// event mouse
+const arrSpecial = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'ShiftLeft', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+function printKey(event) {
+  keys.forEach((element) => {
+    if (event.target === element) {
+      element.classList.add('active');
+      setTimeout(() => {
+        if (element.dataset.key !== 'CapsLock') element.classList.remove('active');
+      }, 200);
+
+      let letter = element.textContent;
+      if (element.dataset.key === 'CapsLock') {
+        if (isCapsLock) {
+          isCapsLock = false;
+          element.classList.remove('active');
+
+          keys.forEach((el) => {
+            if (objLangEn[el.dataset.key]) {
+              const newTextContent = el.textContent.toLowerCase();
+              el.textContent = newTextContent;
+            }
+          });
+        } else {
+          isCapsLock = true;
+          element.classList.add('active');
+          keys.forEach((el) => {
+            if (objLangEn[el.dataset.key]) {
+              const newTextContent = el.textContent.toUpperCase();
+              el.textContent = newTextContent;
+            }
+          });
+        }
+      }
+      if (!isCapsLock) {
+        letter = letter.toLowerCase();
+      }
+
+      const text = textarea.value;
+      if (!arrSpecial.includes(element.dataset.key)) {
+        textarea.value = text + letter;
+      }
+      if (element.dataset.key === 'Enter') {
+        textarea.value = `${text}\n`;
+      }
+      if (element.dataset.key === 'Backspace') {
+        const cursorStart = textarea.selectionStart;
+        if (cursorStart === textarea.value.length) {
+          textarea.value = textarea.value.substring(0, cursorStart - 1);
+        } else if (cursorStart > 0) {
+          textarea.value = textarea.value.substring(0, cursorStart - 1)
+            + textarea.value.substring(cursorStart);
+          textarea.selectionStart = cursorStart - 1;
+          textarea.selectionEnd = cursorStart - 1;
+        }
+      }
+      if (element.dataset.key === 'Delete') {
+        const cursorStart = textarea.selectionStart;
+        if (cursorStart < textarea.value.length) {
+          textarea.value = textarea.value.substring(0, cursorStart)
+            + textarea.value.substring(cursorStart + 1);
+          textarea.selectionStart = cursorStart;
+          textarea.selectionEnd = cursorStart;
+        }
+      }
+      if (element.dataset.key === 'Tab') {
+        textarea.value = `${text}  `;
+      }
+      if (element.dataset.key === 'ArrowLeft') {
+        textarea.value = text + String.fromCharCode(0x25C4);
+      }
+      if (element.dataset.key === 'ArrowDown') {
+        textarea.value = text + String.fromCharCode(0x25BC);
+      }
+      if (element.dataset.key === 'ArrowUp') {
+        textarea.value = text + String.fromCharCode(0x25B2);
+      }
+
+      if (element.dataset.key === 'ArrowRight') {
+        textarea.value = text + String.fromCharCode(0x25BA);
+      }
+
+      textarea.focus();
+    }
+  });
+}
+window.addEventListener('click', printKey);
